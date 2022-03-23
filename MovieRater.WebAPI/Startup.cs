c62bfe27e94ee3378de6e8fbb2bb2e276064f2a2
Bryon <pearson.bryon90@gmail.com>
2022-03-23 15:28:14 -0700

    Bryon's Branch

--- MovieRater.WebAPI/Startup.cs
+++ MovieRater.WebAPI/Startup.cs
@@ -21,6 +21,8 @@ namespace MovieRater.WebAPI
             Configuration = configuration;
         }
 
+        // TeamBuilding 101
+
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
