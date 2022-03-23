608702ea71176111c4aec88c64cb786809edc653
Tony Albacete <tbalbacete@gmail.com>
2022-03-23 16:05:58 -0400

    resolve comments

--- MovieRater.WebAPI/Startup.cs
+++ MovieRater.WebAPI/Startup.cs
@@ -21,6 +21,12 @@ namespace MovieRater.WebAPI
             Configuration = configuration;
         }
 
+            
+            Configuration = configuration;
+        }
+
+        
+
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
