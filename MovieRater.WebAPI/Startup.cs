0bb6b70f7ca27c6856a1491f15a7d5ce471092c7
Bryon <pearson.bryon90@gmail.com>
2022-03-24 11:56:57 -0700

    adding Application file, Connection Strings, and DB Context

--- MovieRater.WebAPI/Startup.cs
+++ MovieRater.WebAPI/Startup.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using MovieRater.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -11,6 +12,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace MovieRater.WebAPI
 {
@@ -21,13 +23,14 @@ namespace MovieRater.WebAPI
             Configuration = configuration;
         }
 
-        // TeamBuilding 101
-
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            // Add connection string and DbContext setup
+            var connectionString = Configuration.GetConnectionString("DefaultConnection");
+            services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(connectionString));
 
             services.AddControllers();
             services.AddSwaggerGen(c =>
