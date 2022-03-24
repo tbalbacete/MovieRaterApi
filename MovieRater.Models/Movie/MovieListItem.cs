6cc4e24229fb4713eb0c22a5301ddfc1586d5da7
Eilers Fam <alexeilers@Alexs-iMac.local>
2022-03-24 16:50:12 -0400

    first final draft

--- MovieRater.Models/Movie/MovieListItem.cs
+++ MovieRater.Models/Movie/MovieListItem.cs
@@ -9,6 +9,7 @@ namespace MovieRater.Models.Movie
     {
         public int Id { get; set; }
         public string Title { get; set; }
+        public string Genre { get; set; }
         public DateTimeOffset CreatedUtc { get; set; }
     }
 }
